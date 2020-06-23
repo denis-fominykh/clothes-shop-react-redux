52416e94014b6266591c7b74115a3720c010a86b
denis-fominykh <fominykhdenis@gmail.com>
2020-06-24 01:29:34 +0300

    creating a menu item component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,40 +2,17 @@ import React from 'react';
 
 import './HomePage.scss';
 
+import MenuItem from '../../components/MenuItem';
+
 const HomePage = () => {
   return (
     <div className="homepage">
       <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
+        <MenuItem title={'HATS'} />
+        <MenuItem title={'JACKETS'} />
+        <MenuItem title={'SNEAKERS'} />
+        <MenuItem title={'WOMENS'} />
+        <MenuItem title={'MENS'} />
       </div>
     </div>
   );
