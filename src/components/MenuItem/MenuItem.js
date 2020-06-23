77068a78856b24652dc86dac5514996210a3853d
denis-fominykh <fominykhdenis@gmail.com>
2020-06-24 02:06:11 +0300

    adding a prop-types to component

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './MenuItem.scss';
 
@@ -16,6 +17,12 @@ const MenuItem = ({ title, imageUrl, imageSize }) => {
       </div>
     </div>
   );
+};
+
+MenuItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  imageSize: PropTypes.string.isRequired,
 };
 
 export default MenuItem;
