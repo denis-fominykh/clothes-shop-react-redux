ca51896463887d3393c05a9a292dd1212d700df8
denis-fominykh <fominykhdenis@gmail.com>
2020-06-25 16:33:20 +0300

    creating an error boundary component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.scss';
 
+import ErrorBoundary from './components/ErrorBoundary';
 import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-        </Switch>
-      </div>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </div>
+      </Router>
+    </ErrorBoundary>
   );
 };
 
